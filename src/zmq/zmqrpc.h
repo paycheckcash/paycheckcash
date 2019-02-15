3911c85d995282ce81f936df2e9379f6b988f09d
paycheckcash <47651673+paycheckcash@users.noreply.github.com>
2019-02-14 19:50:15 -0800

    Add files via upload

---
+++ src/zmq/zmqrpc.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2018 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef PAYCHECKCASH_ZMQ_ZMQRPC_H
+#define PAYCHECKCASH_ZMQ_ZMQRPC_H
+
+class CRPCTable;
+
+void RegisterZMQRPCCommands(CRPCTable& t);
+
+#endif // PAYCHECKCASH_ZMQ_ZMRRPC_H
