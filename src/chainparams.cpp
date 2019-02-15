7c6fac4edc82df719e217cfa28aca0b374bf2644
paycheckcash <47651673+paycheckcash@users.noreply.github.com>
2019-02-14 22:50:06 -0800

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -170,13 +170,9 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        vSeeds.emplace_back("seed.paycheckcash.sipa.be");          // Pieter Wuille, only supports x1, x5, x9, and xd
-        vSeeds.emplace_back("dnsseed.bluematt.me");                // Matt Corallo, only supports x9
-        vSeeds.emplace_back("dnsseed.paycheckcash.dashjr.org");    // Luke Dashjr
-        vSeeds.emplace_back("seed.paycheckcashstats.com");         // Christian Decker, supports x1 - xf
-        vSeeds.emplace_back("seed.paycheckcash.jonasschnelli.ch"); // Jonas Schnelli, only supports x1, x5, x9, and xd
-        vSeeds.emplace_back("seed.btc.petertodd.org");             // Peter Todd, only supports x1, x5, x9, and xd
-        vSeeds.emplace_back("seed.paycheckcash.sprovoost.nl");     // Sjors Provoost
+        // vSeeds.emplace_back("seed.paycheckcash.sipa.be");          
+        // vSeeds.emplace_back("dnsseed.bluematt.me");                
+     
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 55);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 38);
