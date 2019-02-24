4abfccddee8d838238517c697317784d788dc5ff
paycheckcash <47651673+paycheckcash@users.noreply.github.com>
2019-02-23 23:02:01 -0800

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -107,7 +107,7 @@ public:
         consensus.nSubsidyHalvingInterval = 210000;
         consensus.BIP16Exception = uint256S("0x00000000000002dc756eebf4f49723ed8d30cc28a5f108eb94b1ba88ac4f9c22");
 
-        consensus.nPostForkSubsidyHalvingInterval = 105000;
+        consensus.nPostForkSubsidyHalvingInterval = 40000;
 
         consensus.BIP34Height = select_activation_height(227931);
         consensus.BIP34Hash = uint256S("0x000000000000024b89b42a942fe0d9fea3bb44ab7bd1b19115dd6a759c0808b8");
