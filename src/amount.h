a352da203564491ebc41570933adfc59f277d587
Paycheck cash team <47651673+paycheckcash@users.noreply.github.com>
2019-03-06 02:02:25 -0800

    Update amount.h

--- src/amount.h
+++ src/amount.h
@@ -11,8 +11,8 @@
 /** Amount in satoshis (Can be negative) */
 typedef int64_t CAmount;
 
-static const CAmount COIN = 100;
-static const CAmount CENT = 1;
+static const CAmount COIN = 100000000;
+static const CAmount CENT = 1000000;
 
 /** No amount larger than this (in satoshi) is valid.
  *
@@ -23,7 +23,7 @@ static const CAmount CENT = 1;
  * critical; in unusual circumstances like a(nother) overflow bug that allowed
  * for the creation of coins out of thin air modification could lead to a fork.
  * */
-static const CAmount MAX_MONEY = 1000000000000 * COIN;
+static const CAmount MAX_MONEY = 21000000 * COIN;
 inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 
 #endif //  PAYCHECKCASH_AMOUNT_H
